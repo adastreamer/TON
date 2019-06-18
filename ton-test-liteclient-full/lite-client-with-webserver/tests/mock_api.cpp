deccd2a940b7f0fa74bd0e4b4e87bd451649dcff
Enziferum <alexraag@yandex.ru>
2019-06-18 23:47:42 +0300

    added mock testsfor new HttpServer

---
+++ ton-test-liteclient-full/lite-client-with-webserver/tests/mock_api.cpp
@@ -0,0 +1,18 @@
+#include "mock_api.hpp"
+
+MockApi::MockApi(){
+	
+}
+
+
+void mockGetFoo(){
+	
+}
+
+void web_error_response(std::shared_ptr<SimpleWeb::Response> response, std::string msg){
+	
+}
+
+void web_success_response(std::shared_ptr<SimpleWeb::Response> response, std::string msg){
+	
+}
